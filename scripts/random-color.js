a450179a80fe6deeb7d4724c3c205c8982973336
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-02-18 22:00:23 +0600

    Complete random color on cards

---
+++ scripts/random-color.js
@@ -0,0 +1,31 @@
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+document.getElementById('triangle-shape').addEventListener('mouseover', function(){
+    document.getElementById('triangle-shape').style.backgroundColor = randomColor();
+})
+
+document.getElementById('rectangle-shape').addEventListener('mouseover', function(){
+    document.getElementById('rectangle-shape').style.backgroundColor = randomColor();
+})
+
+document.getElementById('parallelogram-shape').addEventListener('mouseover', function(){
+    document.getElementById('parallelogram-shape').style.backgroundColor = randomColor();
+})
+
+document.getElementById('rhombus-shape').addEventListener('mouseover', function(){
+    document.getElementById('rhombus-shape').style.backgroundColor = randomColor();
+})
+
+document.getElementById('pentagon-shape').addEventListener('mouseover', function(){
+    document.getElementById('pentagon-shape').style.backgroundColor = randomColor();
+})
+
+document.getElementById('ellipse-shape').addEventListener('mouseover', function(){
+    document.getElementById('ellipse-shape').style.backgroundColor = randomColor();
+})
